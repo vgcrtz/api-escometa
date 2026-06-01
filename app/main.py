d122089a0566962a154912a014fd4a54a8bddf5d
Omar Jesús Vázquez Sánchez <omar_jv.s@hotmail.com>
2026-06-01 00:24:33 -0600

    Cosas solucionadas después de merge

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from app.routes.academico import router as academico_router
 from app.routes.asistencias import router as asistencias_router
 from app.routes.comunicacion import router as comunicacion_router
 from app.routes.chatbot import router as chatbot_router
+from app.routes.chat import router as chat_router
 
 app = FastAPI(title="ESCOMETA API")
 
@@ -25,6 +26,7 @@ app.include_router(usuarios_router)
 app.include_router(academico_router)
 app.include_router(asistencias_router)
 app.include_router(comunicacion_router)
+app.include_router(chat_router)
 app.include_router(chatbot_router)
 
 
